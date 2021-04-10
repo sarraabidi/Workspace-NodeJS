2bf6e1bb417f33972299df735f05b1ab4f7a78cc
sarraabidi <sarra.abidi@esprit.tn>
2021-04-10 12:54:50 +0200

    mmm

--- socket/index.js
+++ socket/index.js
@@ -1,39 +1,18 @@
-
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
-
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
 
-      socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-      });
-      
-      });
-
-
-    });
-
-
-
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-}); 
-
-
-/*
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});*/
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
